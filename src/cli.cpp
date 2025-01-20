8be47351224f61c17982fccbbc933a1d28e909fc
TheoW03 <21theowillis@gmail.com>
2025-01-20 13:28:56 -0500

    made CLI more verbose

--- src/cli.cpp
+++ src/cli.cpp
@@ -19,11 +19,11 @@ Output parse_cmd(std::vector<std::string> args)
     std::map<std::string,
              Parse_F>
         option_handlers;
-    option_handlers["o"] = {[](std::string &value, Output &o)
-                            {
-                                o.output_files.push_back(value);
-                            },
-                            "output file", 1};
+    option_handlers["output-file"] = {[](std::string &value, Output &o)
+                                      {
+                                          o.output_files.push_back(value);
+                                      },
+                                      "output file", 1};
     option_handlers["print-llvm"] = {[](std::string &value, Output &o)
                                      {
                                          o.print_llvm = 1;
@@ -45,25 +45,41 @@ Output parse_cmd(std::vector<std::string> args)
 
                                    for (const auto &[key, value] : option_handlers)
                                    {
-                                       std::cout << "   -" << key << ": " << value.help_text << std::endl; // Access the key
+                                       std::cout << "   --" << key << ": " << value.help_text << std::endl; // Access the key
                                        std::cout << "" << std::endl;
                                    }
                                },
                                "cli documentation", 0};
-    option_handlers["v"] = {[](std::string &value, Output &o)
-                            {
-                                std::cout << "1.0.0" << std::endl;
-                            },
-                            "prints version", 0};
+    option_handlers["version"] = {[](std::string &value, Output &o)
+                                  {
+                                      std::cout << "1.0.0" << std::endl;
+                                  },
+                                  "prints version", 0};
+    option_handlers["linker-off"] = {[](std::string &value, Output &o) {
+
+                                     },
+                                     "out puts an non linked binary file", 0};
+    option_handlers["assemble-off"] = {[](std::string &value, Output &o) {
+
+                                       },
+                                       "out puts a .s/.asm file", 0};
+    option_handlers["ir-stage-off"] = {[](std::string &value, Output &o) {
+
+                                       },
+                                       "out puts an unoptimized ir file", 0};
+    option_handlers["optimize"] = {[](std::string &value, Output &o) {
+
+                                   },
+                                   "optimization level 0 being the lowest 3 being the heighest", 0};
 
     // func_c(2017);
     std::optional<Parse_F> current;
     for (int i = 0; i < args.size(); i++)
     {
 
-        if (args[i].at(0) == '-')
+        if (args[i].at(0) == '-' && args[i].at(1) == '-')
         {
-            optop = args[i].substr(1, args[i].size());
+            optop = args[i].substr(2, args[i].size());
             if (option_handlers.find(optop) != option_handlers.end())
             {
                 current = option_handlers[optop];
