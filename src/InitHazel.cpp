2e544e9e2b516f544b865d396140017fa7ac8d4e
TheoW03 <21theowillis@gmail.com>
2025-06-17 11:56:03 -0400

    Test

--- src/InitHazel.cpp
+++ src/InitHazel.cpp
@@ -132,7 +132,7 @@ void runPasses(std::shared_ptr<ProgramNode> node, Output cli)
     std::cout << "" << std::endl;
     auto typechecker = std::make_shared<TypeCheckerVistor>(IntermediateScope(semantic->modules));
     node->Accept(typechecker.get());
-    std::cout << "hazelc: checking types" << std::endl;
+    std::cout << "hazelc: checking types" << std::endl; //
 
     node->Accept(std::make_shared<ConstantFoldingVisitor>().get());
 
